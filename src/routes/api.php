da555a2b116216563db603c164f7bd8200e246d9
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-03-27 15:42:31 +0100

    J'ai ajouter les routes du profiles de client et de freelance

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,7 +3,18 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProfileController;
 
+// profile routes
+Route::middleware("auth:sanctum")->group(function () {
+
+    Route::get("/profile/me", [ProfileController::class, "myProfile"]);
+
+    Route::put("/profile/freelance", [ProfileController::class, "updateFreelancerProfile"]);
+    Route::put("/profile/client", [ProfileController::class, "updateClientProfile"]);
+
+});
+// auth routes
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
